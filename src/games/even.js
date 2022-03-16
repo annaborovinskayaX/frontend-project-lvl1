8411d929fdbddd48961bc6b7aaf3f4f68bfa73b5
annaborovinskaya <annaborovin@gmail.com>
2022-03-16 17:51:49 +0300

    Update even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,37 +1,15 @@
-import readlineSync from 'readline-sync';
+import { getRandomInt, gameFrame } from '../index.js';
 
-const getRandomInt = (min, max) => {
-  const min1 = Math.ceil(min);
-  const max1 = Math.floor(max);
-  return Math.floor(Math.random() * (max1 - min1)) + min1;
-};
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const evenGame = () => {
-  console.log('Welcome to the Brain Games!');
-
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  const roundsCount = 3;
+const gameInfo = () => {
   const isEven = (number) => number % 2 === 0;
-
-  for (let i = 0; i < roundsCount; i += 1) {
-    const number = getRandomInt(1, 1000);
-    console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    const answer = isEven(number) ? 'yes' : 'no';
-
-    if (userAnswer === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${answer}`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-
-  console.log(`Congratulations, ${userName}!`);
+  const number = getRandomInt(1, 1000);
+  const answer = isEven(number) ? 'yes' : 'no';
+  const question = `${number}`;
+  return [question, answer];
 };
+
+const evenGame = () => gameFrame(rules, gameInfo);
+
 export default evenGame;
